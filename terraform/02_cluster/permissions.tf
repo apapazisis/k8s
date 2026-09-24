eb86f5b2a9134fcc637aa2080aea015e155e5e20
Apostolos Papazisis <info@apapazisis.de>
2026-09-25 01:01:30 +0200

    Add GitHub Actions IAM role and access policy associations for EKS

--- terraform/02_cluster/permissions.tf
+++ terraform/02_cluster/permissions.tf
@@ -28,3 +28,27 @@ resource "aws_eks_access_policy_association" "sso_admin_assoc_admin" {
 
   depends_on = [aws_eks_access_entry.sso_admin_entry]
 }
+
+####
+
+data "aws_iam_role" "github_actions" {
+  name = "GitHubActionsIAMRole"
+}
+
+resource "aws_eks_access_entry" "github_actions" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = data.aws_iam_role.github_actions.arn
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "github_actions_admin" {
+  cluster_name  = aws_eks_cluster.main.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = data.aws_iam_role.github_actions.arn
+
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [aws_eks_access_entry.github_actions]
+}
