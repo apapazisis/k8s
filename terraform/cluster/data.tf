08c1b3c9a77badbe6d6e3645dbd4fa14be84567e
Apostolos Papazisis <info@apapazisis.de>
2026-09-24 18:25:14 +0200

    Add example configuration for AWS EKS identity provider

--- terraform/cluster/data.tf
+++ terraform/cluster/data.tf
@@ -25,3 +25,14 @@ data "aws_eks_addon_version" "eks_pod_identity_agent" {
 data "aws_iam_openid_connect_provider" "eks" {
   url = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
+
+
+# resource "aws_eks_identity_provider_config" "example" {
+#   cluster_name = aws_eks_cluster.example.name
+
+#   oidc {
+#     client_id                     = "your client_id"
+#     identity_provider_config_name = "example"
+#     issuer_url                    = "your issuer_url"
+#   }
+# }
